26c225cb8a8eec2f71e8d83d3763bb68fdb7b271
LifeSteal <166237187+LlfeSteal@users.noreply.github.com>
2021-12-01 18:44:00 +0100

    [UPDATE] Ajout des methode du speech-recognition service responsable de recuperer ce qui est dit par l'utilisateur et le stocker

--- src/app/services/speech-recognition.service.ts
+++ src/app/services/speech-recognition.service.ts
@@ -1,9 +1,36 @@
-import { Injectable } from '@angular/core';
+import {Injectable} from '@angular/core';
+import {BehaviorSubject} from 'rxjs';
+
+declare const webkitSpeechRecognition: any;
 
 @Injectable({
   providedIn: 'root'
 })
 export class SpeechRecognitionService {
 
-  constructor() { }
+  error = true;
+  recognition = new webkitSpeechRecognition();
+  text = '';
+  subj = new BehaviorSubject<string>(this.text);
+  observable = this.subj.asObservable();
+
+  init(): void {
+    this.recognition.lang = 'fr-FR';
+    this.recognition.addEventListener('result', (e: any) => {
+      if (e.results[0].isFinal) {
+        console.log(e.results[0][0].transcript);
+        this.subj.next(e.results[0][0].transcript);
+        this.stop();
+      }
+    });
+  }
+
+  start(): void {
+    this.recognition.start();
+  }
+
+  stop(): void {
+    this.text = '';
+    this.recognition.stop();
+  }
 }
