17dd876cafe7ddc64d8dec4014e6e5aa32ef0bb4
dorianosaure <contact.dorianosaure@gmail.com>
2021-12-01 14:31:48 +0100

    [UPDATE] Ajout des methodes et attributs du composant todolist-title

--- src/app/todolist-title/todolist-title.component.ts
+++ src/app/todolist-title/todolist-title.component.ts
@@ -1,15 +1,40 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, ElementRef, EventEmitter, Input, Output, ViewChild} from '@angular/core';
 
 @Component({
   selector: 'app-todolist-title',
   templateUrl: './todolist-title.component.html',
   styleUrls: ['./todolist-title.component.scss']
 })
-export class TodolistTitleComponent implements OnInit {
+export class TodolistTitleComponent {
 
-  constructor() { }
+  @Input() title!: string;
+  newValue!: string;
+  editing = false;
+  @Output() updateEmitter: EventEmitter<string> = new EventEmitter<string>();
+  @ViewChild('newTitleInput') newTextInput!: ElementRef<HTMLInputElement>;
 
-  ngOnInit(): void {
+  constructor() {
+    this.newValue = this.title;
   }
 
+  setEditing(newValue: boolean): void {
+    this.editing = newValue;
+    if (newValue) {
+      requestAnimationFrame(
+        () => this.newTextInput.nativeElement.focus()
+      );
+    }
+  }
+
+  toggleMode(): void {
+    this.setEditing(!this.editing);
+    this.newValue = this.title;
+  }
+
+  updateValue(): void {
+    if (this.newValue !== undefined && this.newValue !== '') {
+      this.updateEmitter.emit(this.newValue);
+      this.toggleMode();
+    }
+  }
 }
